9b6fff1263fe55bc13cee6cec2901fb4535ed23d
stephen <tasb00429@gmail.com>
2023-11-18 14:13:29 +0800

    feat: add request page and per_page trait

---
+++ src/Traits/Request/RequestPageTrait.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Stephenchen\Core\Traits\Request;
+
+trait RequestPageTrait
+{
+    /**
+     * Get request validation rules
+     *
+     * @return array
+     */
+    public function getPageRules(): array
+    {
+        return [
+            'per_page' => [
+                'numeric',
+                'min:1',
+                'max:300',
+            ],
+            'page'     => [
+                'numeric',
+                'min:1',
+                // Overflow, should be less than id in database
+                'max:2147483647',
+            ],
+        ];
+    }
+
+    /**
+     * Get request validation error message
+     *
+     * @return array
+     */
+    public function getPageMessages(): array
+    {
+        $page    = ['key' => __('core::message.page')];
+        $perPage = ['key' => __('core::message.per_page')];
+        
+        return [
+            'page.numeric' => __('core::message.validation.numeric', $page),
+            'page.min'     => __('core::message.validation.min', [
+                'key1' => 'page',
+                'key2' => '1',
+            ]),
+            'page.max'     => __('core::message.validation.max', [
+                'key1' => 'page',
+                'key2' => '300',
+            ]),
+
+            'per_page.numeric' => __('core::message.validation.numeric', $perPage),
+            'per_page.min'     => __('core::message.validation.min', [
+                'key1' => 'per_page',
+                'key2' => '1',
+            ]),
+            'per_page.max'     => __('core::message.validation.max', [
+                'key1' => 'per_page',
+                'key2' => '2147483647',
+            ]),
+        ];
+    }
+}
